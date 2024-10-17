bef0862a62fc186512149da1ff53ee4d78205cd6
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-10-17 19:33:41 -0300

    feature:  Project started

---
+++ madelar/src/test/java/com/gustavonascimento/madelar/MadelarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.madelar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadelarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
